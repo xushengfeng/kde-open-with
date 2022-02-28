26adba3041b110b2df0f91e9abc1051bfc3b6fb4
xushengfeng <xushengfeng_zg@163.com>
2022-02-28 20:57:42 +0800

    :sparkles:读取关联程序

--- main.cpp
+++ main.cpp
@@ -4,17 +4,29 @@
 #include <KIO/ApplicationLauncherJob>
 #include <KIO/Job>
 #include <KIO/JobUiDelegate>
+#include <KApplicationTrader>
 #include <QList>
+#include <QMenu>
+#include <QMimeDatabase>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
+    QMimeDatabase db;
+    QMimeType mime = db.mimeTypeForFile("./main.cpp");
+
+    const KService::List offers = KApplicationTrader::queryByMimeType(mime.name());
+    for (const auto &service : offers) {
+        qDebug()<<service->name();
+    }
+//    w.show();
     QList<QUrl> list;
     list << QUrl(argv[1]);
     auto *job = new KIO::ApplicationLauncherJob();
     job->setUrls(list);
     job->setUiDelegate(new KIO::JobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, &w));
-    job->start();
+//    job->start();
     return a.exec();
 }
